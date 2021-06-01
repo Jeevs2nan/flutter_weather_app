77332cf0a6ab68d51aa16548ce7bf86c335958c1
Jeevanandhan M <Jeevs2nan@users.noreply.github.com>
2021-06-01 08:07:38 +0800

    Create Constant.dart

---
+++ jeevanandhan_bestinet_test/lib/util/Constant.dart
@@ -0,0 +1,25 @@
+
+
+class Constant {
+
+  static const String OPEN_WEATHER_API = '4873bb7ab45ce734325ceb342008029b';
+  //static const String OPEN_WEATHER_API = '1edbbccefc05be31cdd6a0067be86b40';
+
+  static const String BASE_URL = 'http://api.openweathermap.org/data/2.5/';
+
+  static const String FORECAST_WITH_LAT = 'forecast?lat=';
+  static const String LONG = '&lon=';
+  static const String APP_ID = '&appid=';
+  static const String FORECAST_WITH_CITY = 'forecast?q=';
+  static const String CELCIUS_PARAMETER = '&units=metric';
+  static const String IMAGE_THUMNAL_URL = 'http://openweathermap.org/img/wn/';
+  static const String WEATHER_WITH_CITY = 'weather?q=';
+  static const String IMAGE_URL_END = '@2x.png';
+
+  static const String FAVORITE_PREFERENCE_KEY = 'Favorite';
+
+
+  //http://openweathermap.org/img/wn/10d@2x.png
+
+
+}
